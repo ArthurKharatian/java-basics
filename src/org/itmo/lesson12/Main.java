bcd1fceebcaf213bba2e7440968282add55465ba
Kharatian <kharatian_arthur@mail.ru>
2026-02-05 19:47:38 +0300

    lesson12

---
+++ src/org/itmo/lesson12/Main.java
@@ -0,0 +1,51 @@
+package org.itmo.lesson12;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+
+public class Main {
+    public static void main(String[] args) {
+//        Thread myThread = new MyThread();
+//        Thread myRunnable = new Thread(new MyRunnable());
+//
+//        myThread.start();
+//        try {
+//            myThread.join();
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//        myRunnable.start();
+//        myThread.run();
+
+
+//        CountDownLatch
+
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+//        new ThreadExample();
+
+//
+//        ExecutorService executorService = Executors.newFixedThreadPool(10);
+//        ForkJoinPool
+    }
+}
---
+++ src/org/itmo/lesson12/MyRunnable.java
@@ -0,0 +1,12 @@
+package org.itmo.lesson12;
+
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println("My runnable is running " + Thread.currentThread().getName());
+            i++;
+        }
+    }
+}
